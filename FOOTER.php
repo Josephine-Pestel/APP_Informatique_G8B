0a92f5c18d0cdc8e7d1e7a05e53eabf2af40199c
ChenuA <60643760+ChenuA@users.noreply.github.com>
2020-03-13 15:17:12 +0100

    ajout des images en tete footer et reglage de la page mesresultats

---
+++ FOOTER.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- En-tête de la page -->
+    <link type="text/css" rel="stylesheet" href="FOOTER.css?t=<? echo time(); ?>" media="all">
+    <!--<link rel="stylesheet" href="P_Accueil_Payen.css" />-->
+    <meta charset="utf-8" />
+    <title>WINK FOR INIFINITE MEASURES</title>
+</head>
+<body>
+
+<footer>
+
+    <div class ="signature">
+        <p class="realisation">  REALISER PAR</p>
+        <img class="logo_WINK" src="images/logowink.png" alt="Logo WINK" />
+    </div>
+
+
+    <div class="bloc_footer1">
+        <div class="texte_footer">
+
+            <h1>À Propos de nous</h1>
+            <p>Wink est une jeune start-up française spécialisée dans la réalisation de tests psychotechniques</p>
+            <p>Les tests sur cette plateforme ont été conçus pour Infinite Measures</p>
+        </div>
+        <a href="#" class="bouton_mail" alt="Envoyer un mail" target="_blank">CONTACTEZ-NOUS</a>
+
+    </div>
+
+    <div class="trait_fin_footer"></div>
+
+    <div class="bloc_footer2">
+        <a href="#" class="cookies"><p>COOKIES</p></a>
+        <a href="#" class="confidentialite"><p>CONFIDENTIALITE</p></a>
+        <a href="#" class="conditions"><p>CONDITIONS</p></a>
+        <a href="#" class="mentions"><p>MENTIONS LEGALES</p></a>
+    </div>
+
+</footer>
+
+</body>
+</html>
