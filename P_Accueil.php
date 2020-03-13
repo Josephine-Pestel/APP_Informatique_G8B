9e66a61cab85e5fcece9dd43d9b8bd9a8b4a2fb9
Josephine-Pestel <josephine.pestel@outlook.com>
2020-03-13 17:19:51 +0100

    code page accueil php et css

---
+++ P_Accueil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- En-tête de la page -->
+    <link type="text/css" rel="stylesheet" href="P_Accueil.css?t=<? echo time(); ?>" media="all">
+    <!--<link rel="stylesheet" href="P_Accueil_Payen.css" />-->
+    <meta charset="utf-8" />
+    <title>WINK FOR INIFINITE MEASURES</title>
+</head>
+
+
+
+<body>
+<!-- Milieu -->
+<!-- Bannière -->
+
+<?php
+require('ENTETE_MENU.php');
+
+?>
+
+<section>
+    <!-- Milieu -->
+    <!-- Bannière -->
+    <div class="banniere_image">
+        <div id="banniere_description">
+            <p>TESTEZ VOS RÉFLEXES AVEC NOS DERNIERS TESTS PSYCHOTECHNIQUES</p>
+        </div>
+    </div>
+</section>
+
+
+
+</body>
+
+<?php
+require('FOOTER.php');
+
+?>
+</html>
