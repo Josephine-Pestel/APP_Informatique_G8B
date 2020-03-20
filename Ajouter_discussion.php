dccca0a392cf5257e70e0c270e6333eed76e09df
linaelle.frid@gmail.com <linaelle.friderich@isep.fr>
2020-03-20 15:17:03 +0100

    ouf

---
+++ Ajouter_discussion.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Ajouter_discussion.css" />
+    <title>WINK FOR INIFINITE MEASURES</title>
+</head>
+
+<body>
+<!-- header et menu -->
+<?php include("ENTETE_MENU.php"); ?>
+
+
+<!--  ---------------------- FORMULAIRE DE DISCUSSION---------------------------------------   -->
+<section>
+    <h1 id="titre_forum"> FORUM </h1>
+    <div id="description">
+        <p>Remplissez le formulaire afin de lancer une nouvelle discussion  </p>
+    </div>
+
+    <div class="formulaire">
+    <form method="post" action="traitement.php">
+        <p>
+            <label for="titre">Entrez votre question : </label><br />
+            <textarea name="titre" id="titre"></textarea>
+        </p>
+    </form>
+
+    <form method="post" action="traitement.php">
+        <p>
+            <label for="Theme">Thème de la discussion : </label><br />
+            <select name="Theme" id="Theme">
+                <option value="Température">Température</option>
+                <option value="Capteur_cardiaque">Capteur cardiaque</option>
+                <option value="Reconnaissance_tonalite">Reconnaissance de tonalité</option>
+                <option value="Temps_recation_visuel">Temps de réaction à un stimulus visuel</option>
+                <option value="Temps_reaction_sonore">Temps de réaction à un stimulus sonore</option>
+                <option value="Authentification">Authentification</option>
+                <option value="Mon_compte">Mon compte</option>
+                <option value="Langue">Langue</option>
+                <option value="autres">autres</option>
+            </select>
+        </p>
+    </form>
+    </div>
+
+    <button class="bouton_discussion">Créer le fil de discussion</button>
+
+
+
+
+
+</section>
+
+<!------------------------------Fin de la section ------------------------------------->
+<footer>
+    <?php include("FOOTER.php"); ?>
+</footer>
+
+
+</body>
+</html>
---
+++ P_Administration.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- En-tête de la page -->
+    <link type="text/css" rel="stylesheet" href="P_Administrateur.css?t=<? echo time(); ?>" media="all">
+    <!--<link rel="stylesheet" href="P_Accueil_Payen.css" />-->
+    <meta charset="utf-8" />
+    <title>WINK FOR INIFINITE MEASURES</title>
+</head>
+
+
+
+<body>
+
+<!-- Appel En-Tete et Menu  + TEST-->
+
+<?php
+require('ENTETE_MENU.php');
+
+?>
+
+<h1 class="titre_admin"> PORTAIL DE L'ADMINISTRATEUR</h1>
+
+<button class="connexion"> CONNEXION</button>
+
+<!-- Milieu -->
+
+<section class="possibilites">
+    <button class="forum"> GERER LE FORUM</button>
+    <button class="FAQ"> GERER LA FAQ</button>
+    <button class="utilisateur"> GERER LES UTILISATEURS</button>
+</section>
+
+</body>
+
+<!-- Appel Footer -->
+<?php
+require('FOOTER.php');
+
+?>
+
+</html>
+
