2b2091dfb0a17d6287118fd69554d35e76dc45b2
Josephine-Pestel <josephine.pestel@outlook.com>
2020-04-21 10:46:23 +0200

    page acceuil sans compte

---
+++ View/HTML_Files/ENTETE_MENU_SansCompte.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- En-tête de la page -->
+    <link type="text/css" rel="stylesheet" href="../CSS_files/ENTETE_MENU_SansCompte.css?t=<? echo time(); ?>" media="all">
+    <!--<link rel="stylesheet" href="P_Accueil_Payen.css" />-->
+    <meta charset="utf-8" />
+    <title>WINK FOR INIFINITE MEASURES</title>
+</head>
+
+
+<body>
+
+<header>
+    <div class="taille_menu"> </div>
+    <a href="" class="bouton_langue" alt="Changer la langue" target="_blank">ENGLISH</a>
+    <h1 class="titre">INFINITE MEASURES</h1>
+
+    <!-- Petits icones -->
+    <div class="icones">
+        <a href="#" class="bouton_user"><img src="../../images/iconuser.png" alt="" /></a>
+        <a href="mailto:alexandre.payen@lilalions.com" class="bouton_contact"><img src="../../images/iconmail.png" alt="" /></a>
+    </div>
+
+</header>
+
+<div class="trait_fin_menu"></div>
+
+
+
+<div class="bandeau_menu">
+    <img class="logo_ininite_measures" src="../../images/logoinfinitemeasures.png" alt="Logo Infinite Measures" />
+    <nav>
+        <ul>
+            <li><a href="AccueilAdmin.php">Accueil</a></li>
+            <li><a href="Systeme.php">Notre Système</a></li>
+            <li><a href="ForumUtilisateur.php">Forum</a></li>
+            <li><a href="P_Connexion.php">Mon compte</a></li>
+            <li><a href="P_FAQ.html">FAQ</a></li>
+        </ul>
+    </nav>
+
+</div>
+
+
+
+
+
+
+</body>
+</html>
