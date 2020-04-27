bb3ae1e89afe5fb6fe315b75cc172a84429173ea
linaelle.frid@gmail.com <linaelle.friderich@isep.fr>
2020-04-27 17:10:08 +0200

    creation fichier controller editer profil gestionnaire

--- View/HTML_Files/Gestionnaire/EditerProfilGestionnaireController.php
+++ View/HTML_Files/Gestionnaire/EditerProfilGestionnaireController.php
@@ -1,1 +1,29 @@
 <?php
+session_start();
+//appelle du model editer profil BDD
+require('EditerProfilGestionnaireBdd.php');
+
+if($_POST['nom']){
+    updatePrenom($_SESSION['email']);
+    $_SESSION['nom']=$_POST['nom'];
+    header('Location:MonCompteGestionnaire.php');
+}
+if($_POST['email']){
+    updateNom($_SESSION['email']);
+    $_SESSION['email']=$_POST['nvEmail'];
+    header('Location:MonCompteGestionnaire.php');
+}
+
+if($_POST['NumeroTel']){
+    updateNaissance($_SESSION['email']);
+    $_SESSION['NumeroTel']=$_POST['NumeroTel'];
+    header('Location:MonCompteGestionnaire.php');
+}
+if($_POST['Domaine']){
+    updateNaissance($_SESSION['email']);
+    $_SESSION['Domaine']=$_POST['Domaine'];
+    header('Location:MonCompteGestionnaire.php');
+}
+else{
+    echo "tu n'a rien rempli";
+}
