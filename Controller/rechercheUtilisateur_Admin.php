987264227aa2446e777dc62f78d3f98495e10a88
Josephine-Pestel <josephine.pestel@outlook.com>
2020-05-03 18:44:26 +0200

    finalisation barre de recherche admin

---
+++ Controller/rechercheUtilisateur_Admin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+</head>
+
+<?php
+require('../../../Model/rechercheUtilisateur.php');
+$recupUtilisateurs = recupUtilisateurs();
+
+while ($donnees = $recupUtilisateurs->fetch())
+{
+    ?>
+    <tr>
+        <th><p class="utilisateur"><?php echo $donnees['email'] ?></p></th>
+        <th><p class="Date"><?php echo $donnees['nom'] ?></p></th>
+        <th><p class="Score"><?php echo $donnees['prenom'] ?></p></th>
+        <th><p class="Score"><?php echo $donnees['date_naissance'] ?></p></th>
+        <th><p class="Score"><?php echo $donnees['date_inscription'] ?></p></th>
+    </tr>
+    <?php
+}
+
+$recupUtilisateurs->closeCursor();
+
+?>
