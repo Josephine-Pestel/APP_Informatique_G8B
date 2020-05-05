313bce35b21ce6ad8355169bd65c4f570d7e75c0
linaelle.frid@gmail.com <linaelle.friderich@isep.fr>
2020-05-05 17:23:55 +0200

    finalisation du design de la page

--- View/HTML_Files/Gestionnaire/Gestionnaire_editer_profil_utilisateurs.php
+++ View/HTML_Files/Gestionnaire/Gestionnaire_editer_profil_utilisateurs.php
@@ -14,28 +14,35 @@ require('ENTETE_MENU_Gestionnaire.php');
 
 ?>
 
+<div class="all">
 
-<div class="FormulaireEditer">
-    <h1>Veuillez indiquer l'adresse mail de l'utilisateur que vous souhaitez modifier :  </h1>
-    <form action="Gestionnaire_editer_profil_utilisateurs_MODEL.php" method="POST">
-        <p><label>Identifiant de l'utilisateur : <input type ="text" name="email"/></label></p>
-        <h1> Remplissez les données que vous souhaitez modifier sur cette utilisateur : </h1>
-        <p><label> Nom : <input  type="text" name="nom"></label></p>
-        <p><label> Prénom : <input type="text" name="prenom"></label></p>
-        <p><label> Mot de passe : <input type="text" name="mdp"></label></p>
-        <p><label> Date d'inscription : <input type="date" name="date_inscription"></label></p>
-        <p><input type="submit" /></p>
+    <div class="FormulaireEditer">
+        <h1>Veuillez indiquer l'adresse mail de l'utilisateur que vous souhaitez modifier :  </h1>
+        <form action="Gestionnaire_editer_profil_utilisateurs_MODEL.php" method="POST">
+            <p><label>Identifiant de l'utilisateur : <input type ="text" name="email"/></label></p>
+            <h1> Remplissez les données que vous souhaitez modifier sur cette utilisateur : </h1>
+            <p><label> Nom : <input  type="text" name="nom"></label></p>
+            <p><label> Prénom : <input type="text" name="prenom"></label></p>
+            <p><label> Mot de passe : <input type="text" name="mdp"></label></p>
+            <p><label> Date de naissance : <input type="date" name="date_naissance"></label></p>
+            <p><label> Date d'inscription : <input type="date" name="date_inscription"></label></p>
+            <p><input type="submit" value="Valider les modifications"/></p>
+    </div>
+
+    <div class="FormulaireSupprimer">
+        <h1 class="titre_suppression"> Si vous voulez supprimer un utilisateur rentrez son identifiant :</h1>
+        <form action="Gestionnaire_editer_profil_utilisateurs_MODEL.php" method="POST">
+            <p><label>Identifiant de l'utilisateur : <input type ="text" name="email"/></label></p>
+            <p><input type="submit" value="Supprimer cette utilisateur"/></p>
+    </div>
 </div>
 
-<div class="FormulaireSupprimer">
-    <h1 class="titre_suppression"> Si vous voulez supprimer un utilisateur rentrez son identifiant :</h1>
-    <form action="Gestionnaire_editer_profil_utilisateurs_MODEL.php" method="POST">
-        <p><label>Identifiant de l'utilisateur : <input type ="text" name="email"/></label></p>
-        <p><input type="submit" name="Supprimer cette utilisateur"/></p>
-        <a href="MonCompteGestionnaire.php">
-            <button class="bouton_retour">RETOUR </button>
-        </a>
-</div>
+
+<a href="MonCompteGestionnaire.php">
+    <button class="bouton_retour">RETOUR </button>
+</a>
+
+
 </body>
 <?php
 require('../FOOTER.php');
