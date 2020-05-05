a7ca9fda64ca5caba835cbbe4309e154ca02dd49
linaelle.frid@gmail.com <linaelle.friderich@isep.fr>
2020-05-05 17:42:07 +0200

    fonction model terminé

--- View/HTML_Files/Gestionnaire/Gestionnaire_editer_profil_utilisateurs_MODEL.php
+++ View/HTML_Files/Gestionnaire/Gestionnaire_editer_profil_utilisateurs_MODEL.php
@@ -1,1 +1,63 @@
 <?php
+
+try {
+    $bdd= new PDO( 'mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+} catch (Exception $e){
+    die('Erreur:'.$e->getMessage());
+}
+
+// Requête pour modifier le nom
+    function updateNom($_POST['email'])
+    {
+        $bdd= new PDO( 'mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+        $req = $bdd->prepare('UPDATE utilisateurs SET nom = :nom WHERE email = :email ');
+        $req->execute(array(
+            'nom' => $_POST['nom'],
+            'email' =>$_POST['email'],
+        $req->closeCursor();
+    }
+    // Requête pour modifier le prénom
+    function updatePrenom($_POST['email'])
+    {
+        $bdd= new PDO( 'mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+        $req = $bdd->prepare('UPDATE utilisateurs SET prenom = :prenom WHERE email = :email ');
+        $req->execute(array(
+            'prenom' => $_POST['prenom'],
+            'email' =>$_POST['email'],
+            $req->closeCursor();
+    }
+
+    // Requête pour modifier le mot de passe
+    function updateMdp($_POST['email'])
+    {
+        $bdd= new PDO( 'mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+        $req = $bdd->prepare('UPDATE utilisateurs SET mdp = :mdp WHERE email = :email ');
+        $req->execute(array(
+            'mdp' => $_POST['mdp'],
+            'email' =>$_POST['email'],
+            $req->closeCursor();
+    }
+    // Requête pour modifier la date d'inscription
+    function updateDateInscrip($_POST['email'])
+    {
+        $bdd= new PDO( 'mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+        $req = $bdd->prepare('UPDATE utilisateurs SET date_inscription = :date_inscription WHERE email = :email ');
+        $req->execute(array(
+            'date_inscription' => $_POST['date_inscription'],
+            'email' =>$_POST['email'],
+            $req->closeCursor();
+    }
+
+    // Requête pour modifier la date de naissance
+    function updateDateNaiss$_POST['email'])
+    {
+        $bdd= new PDO( 'mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+        $req = $bdd->prepare('UPDATE utilisateurs SET date_naissance = :date_naissance WHERE email = :email ');
+        $req->execute(array(
+            'date_naissance' => $_POST['date_naissance'],
+            'email' =>$_POST['email'],
+            $req->closeCursor();
+    }
+
+
+?>
