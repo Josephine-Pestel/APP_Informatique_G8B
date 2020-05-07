cde133c7439fb5820fc8cc7872f1f2aa0b7d3345
linaelle.frid@gmail.com <linaelle.friderich@isep.fr>
2020-05-07 17:54:07 +0200

    utilisateur resultat tableau

---
+++ View/HTML_Files/Utilisateur/MesResultatsTableauMODEL.php
@@ -0,0 +1,16 @@
+<?php
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur:' . $e->getMessage());
+}
+/////////////////// On sélectionne les resultats d'un utilisateur////////////////////////////////////////////
+
+function selectionne($email){
+    $bdd= new PDO( 'mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+    $req = $bdd->prepare('SELECT `type`, `score`, `date` FROM `tests` WHERE email=:email');
+    $req->execute(array(
+        'email' => $_emailVar));
+    $req->closeCursor();
+}
