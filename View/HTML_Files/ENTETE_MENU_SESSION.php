1a84738ff5b64fc3ce088935db545784f9a00330
Josephine-Pestel <josephine.pestel@outlook.com>
2020-05-08 23:58:15 +0200

    bouton deconnexion

---
+++ View/HTML_Files/ENTETE_MENU_SESSION.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- En-tête de la page -->
+    <link type="text/css" rel="stylesheet" href="../../CSS_files/ENTETE_MENU.css?t=<? echo time(); ?>" media="all">
+    <!--<link rel="stylesheet" href="P_Accueil_Payen.css" />-->
+    <meta charset="utf-8" />
+    <title>WINK FOR INIFINITE MEASURES</title>
+</head>
+
+
+<body>
+
+<header>
+    <div class="taille_menu"> </div>
+
+
+    <h1 class="titre">INFINITE MEASURES</h1>
+    <a href="http://localhost/APP_Informatique_G8B_ANGLAIS/indexxxx.php" target="_blank">
+        <img class= "anglais" src="../../../images/anglais.png" alt= "nom de ton image">
+    </a>
+
+    <div class="test">
+        <a href="http://localhost/APP_Informatique_G8B_ANGLAIS/indexxxx.php" target="_blank">
+            <img class= "email" src="../../../images/iconmail.png" alt= "nom de ton image">
+        </a>
+        <a href="../deconnexion" class="bouton_deconnexion" alt="Changer la langue">Déconnexion</a>
+    </div>
+
+</header>
+
+<div class="trait_fin_menu"></div>
+
+</body>
+</html>
--- View/HTML_Files/Utilisateur/ENTETE_MENU_Utilisateur.php
+++ View/HTML_Files/Utilisateur/ENTETE_MENU_Utilisateur.php
@@ -1,6 +1,6 @@
 <?php
 //Appel de l'entete_menu général
-require('../ENTETE_MENU.php'); ?>
+require('../ENTETE_MENU_SESSION.php'); ?>
 
 <div class="bandeau_menu">
     <img class="logo_ininite_measures" src="../../../images/logoinfinitemeasures.png" alt="Logo Infinite Measures" />
