28a06f1fecf6997ebb24c75b4ca44c3b0bebb1d1
Josephine-Pestel <josephine.pestel@outlook.com>
2020-05-09 18:28:19 +0200

    faq utilisateur

--- View/HTML_Files/Utilisateur/FAQ_Utilisateur.php
+++ View/HTML_Files/Utilisateur/FAQ_Utilisateur.php
@@ -1,44 +1,73 @@
-
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <!-- En-tête de la page -->
-    <link type="text/css" rel="stylesheet" href="../../CSS_files/P_FAQ.css?t=<? echo time(); ?>" media="all">
+    <link type="text/css" rel="stylesheet" href="../../CSS_files/FAQ.css?t=<? echo time(); ?>" media="all">
     <!--<link rel="stylesheet" href="P_Accueil_Payen.css" />-->
     <meta charset="utf-8" />
     <title>WINK FOR INIFINITE MEASURES</title>
+    <script src="../../Javascript_Files/FAQ.js"></script>
+
 </head>
 
 
 
 <body>
-<title>FAQ</title>
+<!-- Milieu -->
+<!-- Bannière -->
 
 <?php
+session_start();
 require('ENTETE_MENU_Utilisateur.php');
 
 ?>
 
-<section class="faq-section">
-    <input type="checkbox" id="q1">
-    <label for="q1">QUi je suis </label>
-    <p>... Le début de la réponse ...</p>
-    <p>... Suite de la réponse et paragraphes optionnels ..... Suite de la réponse et paragraphes optionnels ..... Suite de la réponse et paragraphes optionnels ..... Suite de la réponse et paragraphes optionnels ..... Suite de la réponse et paragraphes optionnels ..... Suite de la réponse et paragraphes optionnels ..... Suite de la réponse et paragraphes optionnels ...</p>
+<?php
+$bdd= new PDO( 'mysql:host=localhost;dbname=g8b;port=3308;charset=UTF8', 'root', '');
+
+?>
 
 
-    <input type="checkbox" id="q1">
-    <label for="q1">Pourquoi la vie ?</label>
-    <p>... Le début de la réponse ...</p>
-    <p>... Suite de la réponse et paragraphes optionnels ...</p>
-</section>
+<div id="menu">
+    <!-- MENU 1-->
+    <div>
+
+        <?php
+        $req = $bdd->query('SELECT question, reponse FROM faq');
+        while ($donnees = $req->fetch()) { ?>
+            <div class="menu">
+                <div >
+
+                    <p class="titre_question"> &#11166; <?php echo  $donnees['question'] ?></p>
+
+                </div>
+
+                <div id="sousmenu2" >
+                    <div class="sousmenu">
+                        <?php echo  $donnees['reponse'] ?>
+                    </div>
+                </div>
+
+            </div>
+            <?php
+        }
+        $req->closeCursor();
+        ?>
+        </a>
+    </div>
+
+</div>
 
 
 
 </body>
 
+<!-- Appel Footer -->
 <?php
 require('FOOTER_Utilisateur.php');
 
 ?>
+
+
 </html>
