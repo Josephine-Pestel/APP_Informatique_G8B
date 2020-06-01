567be2db6c76d7decf18cf5f4bd2f55608dac1f6
Josephine-Pestel <josephine.pestel@outlook.com>
2020-06-01 20:43:37 +0200

    pb liens  réglé (v2)

--- Controller/EditerProfilGestionnaireController.php
+++ Controller/EditerProfilGestionnaireController.php
@@ -6,27 +6,27 @@ require('../Model/EditerProfilGestionnaireBdd.php');
 if($_POST['nom']){
     updateNom($_SESSION['email']);
     $_SESSION['nom']=$_POST['nom'];
-    header('Location:../view/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
+    header('Location:../View/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
 }
 if($_POST['email']){
     updateEmail($_SESSION['email']);
     $_SESSION['email']=$_POST['email'];
-    header('Location:../view/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
+    header('Location:../View/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
 }
 
 if($_POST['NumeroTel']){
     updateTel($_SESSION['email']);
     $_SESSION['num_tel']=$_POST['NumeroTel'];
-    header('Location:../view/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
+    header('Location:../View/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
 }
 if($_POST['Domaine']){
     updateDomaine($_SESSION['email']);
     $_SESSION['domaine']=$_POST['Domaine'];
-    header('Location:../view/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
+    header('Location:../View/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
 }
 if($_POST['MotdePasse']){
     updateMdp($_SESSION['email']);
-    header('Location:../view/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
+    header('Location:../View/HTML_Files/Gestionnaire/MonCompteGestionnaire.php');
 }
 else{
     echo "tu n'a rien rempli";
